827557253e7368a83c6367d996d941f0b106fc2e
Kartik Sharma <imailkartiksharma@gmail.com>
2025-11-06 23:05:13 +0530

    add websocket

---
+++ apps/ws-server/index.ts
@@ -0,0 +1,12 @@
+const wss = new WebSocketServer({ port: 8080 });
+
+let userCount = 0;
+
+wss.on('connection', (ws) => {
+  userCount += 1;
+  console.log("user connected", userCount);
+
+  socket.on("message", (message) => {
+    console.log("message received: %s", message.toString());
+  })
+})
